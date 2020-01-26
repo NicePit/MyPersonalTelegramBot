1c3e09e4c66e7d2c9c1158571e122d69ed52d244
Barabanov <stanislav.barabanov@getwizer.com>
2020-01-26 21:00:03 +0200

    add bot, credentials and settings

---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '814675195:AAFFx8QBrSqNet5qNTgwagaReaQnPQD4p4A'
+NGROK_URL = ''
+BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
+LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(NGROK_URL)
+TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
+TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id={}&text={}'
