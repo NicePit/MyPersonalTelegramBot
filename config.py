484eb568d835c6ce6eb68c38c257a49ca97833df
Barabanov <stanislav.barabanov@getwizer.com>
2020-01-29 15:17:15 +0200

    change config

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 TOKEN = os.environ['TELEGRAM_SECRET_TOKEN']
 APP_ENDPOINT = os.environ['APP_ENDPOINT']
 BASE_TELEGRAM_URL = 'https://api.telegram.org/bot{}'.format(TOKEN)
-LOCAL_WEBHOOK_ENDPOINT = '{}/webhook'.format(APP_ENDPOINT)
-TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, LOCAL_WEBHOOK_ENDPOINT)
+TELEGRAM_WEBHOOK_ENDPOINT = '{}/webhook'.format(APP_ENDPOINT)
+TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_URL, TELEGRAM_WEBHOOK_ENDPOINT)
 TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_URL + '/sendMessage?chat_id={}&text={}'
