bd981b28679477c354e3495a8a8a39a568e6a603
Barabanov <stanislav.barabanov@getwizer.com>
2020-02-03 15:10:26 +0200

    return True in request to webhook endpoint

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def telegram_post():
     # bot.parse_webhook_data(req)
     # success = bot.action()
     # return jsonify(success=success)
-    return True
+    return {"message": "Request is processed"}
 
 
 if __name__ == '__main__':
